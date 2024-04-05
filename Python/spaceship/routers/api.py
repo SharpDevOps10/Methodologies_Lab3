0a152bf8225e1f6e388c0d38e59de693eff72d0b
SharpDevOps10 <td090833@gmail.com>
2024-04-05 21:07:29 +0300

    Change optimized Dockerfile to unoptimized one

--- Python/spaceship/routers/api.py
+++ Python/spaceship/routers/api.py
@@ -9,5 +9,8 @@ def hello_world() -> dict:
         'first_name': 'Danyil',
         'last_name': 'Tymofeiev',
         'age': 18,
-        'group_code': 'IM-22'
+        'group_code': 'IM-22',
+        'email': 'london@capital.org',
+        'date_of_birth': '2023-09-05 06:25:00.000',
+        'pronouns': 'wee/woo'
     }
